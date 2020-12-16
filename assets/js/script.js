3a0ca667eead387998a834a54ce6c9b62f385214
FRANCESCO <OLIVOTTI>
2020-12-16 21:06:44 +0100

    new version

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,24 @@
-/* Create una todo list usando VueJS.
-Potete dare sfogo alla creativitá e per quanto riguarda l'HTML e il CSS.
-Se non sapere che fare, di seguito trovate uno screenshot.
-Funzionalitá:
-La nostra todo list avrá alcune tasks di default predefinite
-L'utente puó inserire nuove tasks
-Cliccando sulla "X" l'utente puó cancellare una task
-Se non ci sono piu task nella lista, mostrate un messaggio tipo "Nulla da fare"
-Quando l'utente inserisce una task ha due modi per salvarla: o preme il pulsante add o preme il taso Enter della tastiera.
-Attenzione: l'utente non deve inserire tasks vuote ma almeno un tot di caratteri.
-Da tenere a mente:
-Ricordate di scrivere le istruzioni prima di iniziare a lavorare cercate di descrivere in modo sintetico gli step che pensate siano necessari.
-Consultate la documentazione e se siete bloccati, aprite un ticket.
-Il sono a disposizione per i tickets fino alle 13:00 mentre nel pomeriggio Raffa e Fabiola dalle 15:00 alle 18:00.
-La consegna dell'esercizio é domani pomeriggio. Avete oggi e domani per farlo.
-Domani mattina ci vediamo tutti su Zoom alla solita ora 9:30 per un Q&A di un oretta circa cosi rispondo ai vostri dubbi e poi vi lascio lavorare.
-Che vue sia con voi */
-
-
-var todoApp = new Vue({
-  el: '#todoApp',
+var app = new Vue({
+  el: '#app',
   data: {
-    message: 'Booleans to Todo App',
-    addTodoInput: '',
-    ToDoList: [],
-    hasError: false
+    newItem: '',
+    todos: []
   },
-  methods:{
-    addTask: function(){
-      
-      if(!this.addTodoInput){
-        this.hasError = true;
-        return;
+  methods: {
+    addItem: function(event) {
+      //	alert();
+      if (this.newItem == '') return
+      var todo = {
+        item: this.newItem,
+        isDone: false
       }
-      
-      this.hasError = false;
-      
-      this.ToDoList.push({
-        id:this.ToDoList.length+1, 
-        title: this.addTodoInput, 
-        isComplete: false
-      });
-      
-      this.addTodoInput = '';
+
+      this.todos.push(todo)
+      this.newItem = ''
     },
-     
-    
-    updateTask: function(e, list){
-      e.preventDefault();
-      list.title = e.target.innerText;
-      e.target.blur();
+    deleteItem: function(index) {
+    	// alert(index)
+      this.todos.splice(index, 1)
     }
-    
-
-    
-    
   }
-});
-
+})
